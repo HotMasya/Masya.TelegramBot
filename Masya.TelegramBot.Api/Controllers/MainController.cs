f668d52d437e5fe578d641f8a4e15d9b67def5be
Maksym Diachenko <i.am.masya1@gmail.com>
2021-08-06 00:22:49 +0300

    updated main controller

--- Masya.TelegramBot.Api/Controllers/MainController.cs
+++ Masya.TelegramBot.Api/Controllers/MainController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Masya.TelegramBot.Commands.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Masya.TelegramBot.Api.Controllers
 {
@@ -6,10 +8,28 @@ namespace Masya.TelegramBot.Api.Controllers
     [Route("api/[controller]/[action]")]
     public class MainController : ControllerBase
     {
+        private readonly ILogger<MainController> _logger;
+        private readonly IBotService _botService;
+        private readonly ICommandService _commandService;
+
+        public MainController(IBotService botService, ICommandService commandService, ILogger<MainController> logger)
+        {
+            _botService = botService;
+            _commandService = commandService;
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok(new { Message = "Hello from main controller.", Count = 1 });
+            return Ok(
+                new
+                {
+                    Host = _botService.Options.WebhookHost,
+                    Token = _botService.Options.Token,
+                    TotalCommands = _commandService.Commands.Count
+                }
+            );
         }
     }
 }
