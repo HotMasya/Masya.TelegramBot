ae4d3874d1e461f1bd245a052b3c8b8c689879b9
Maksym Diachenko <i.am.masya1@gmail.com>
2021-08-06 00:54:54 +0300

    updated program.cs

--- Masya.TelegramBot.Api/Program.cs
+++ Masya.TelegramBot.Api/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Threading.Tasks;
 using Masya.TelegramBot.Commands.Abstractions;
 using Masya.TelegramBot.Modules;
+using Masya.TelegramBot.DataAccess;
 
 namespace Masya.TelegramBot.Api
 {
@@ -24,6 +25,9 @@ namespace Masya.TelegramBot.Api
             var botService = services.GetRequiredService<IBotService>();
             await commandService.LoadCommandsAsync(typeof(BasicModule).Assembly);
             await botService.SetWebhookAsync();
+            using var scope = services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await ApplicationDbContext.SeedDatabase(dbContext);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
