7e4e15644137146fac510001e6e4b58962d1545d
Maksym Diachenko <i.am.masya1@gmail.com>
2021-08-12 00:37:48 +0300

    init sql folder

---
+++ sql/insert_catetories.sql
@@ -0,0 +1,9 @@
+INSERT INTO Categories(Name, SuperType) VALUES
+(N'Квартира', 1),
+(N'Дом', 2),
+(N'Участок', 4),
+(N'Коммерческая', 8),
+(N'Новостройка', 16),
+(N'Аренда', 32);
+
+SELECT * FROM Categories;
