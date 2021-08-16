f93c89d2d6a951041b60d42efb78873666733e70
Maksym Diachenko <i.am.masya1@gmail.com>
2021-08-16 23:43:28 +0300

    Updated commands controller

--- Masya.TelegramBot.Api/Controllers/CommandsController.cs
+++ Masya.TelegramBot.Api/Controllers/CommandsController.cs
@@ -5,6 +5,8 @@ using Masya.TelegramBot.DataAccess.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Masya.TelegramBot.Api.Controllers
 {
@@ -13,7 +15,6 @@ namespace Masya.TelegramBot.Api.Controllers
     [Route("api/[controller]")]
     public sealed class CommandsController : ControllerBase
     {
-
         private readonly ApplicationDbContext _dbContext;
 
         public CommandsController(ApplicationDbContext dbContext)
@@ -24,52 +25,62 @@ namespace Masya.TelegramBot.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var commands = await _dbContext.Commands.Include(c => c.Aliases).ToListAsync();
-            return Ok(commands);
+            var commands = await _dbContext.Commands
+                .Include(c => c.Aliases)
+                .ToListAsync();
+            return Ok(commands.Where(c => c.ParentId == null));
         }
 
-        [HttpPost("add")]
-        public async Task<IActionResult> AddCommandAsync(CommandDto dto)
+        [HttpPut("save")]
+        public async Task<IActionResult> SaveCommandsAsync(IEnumerable<Command> commands)
         {
-            Command newCommand = new Command
-            {
-                Name = dto.Name,
-                Permission = dto.Permission,
-                IsEnabled = dto.IsEnabled,
-                DisplayInMenu = dto.DisplayInMenu
-            };
-
-            foreach (var alias in dto.Aliases)
-            {
-                newCommand.Aliases.Add(
-                    new Command()
-                    {
-                        Name = dto.Name,
-                        Permission = dto.Permission,
-                        IsEnabled = dto.IsEnabled,
-                        DisplayInMenu = dto.DisplayInMenu
-                    }
-                );
-            }
-
-            _dbContext.Commands.Add(newCommand);
-            await _dbContext.SaveChangesAsync();
-
-            return Created("api/controller/add", dto);
-        }
-
-        [HttpDelete("delete")]
-        public async Task<IActionResult> DeleteCommandAsync(int id)
-        {
-            var command = await _dbContext.Commands.FirstOrDefaultAsync(c => c.Id == id);
-            if (command == null)
-            {
-                return BadRequest(new MessageResponseDto("Command not found."));
-            }
-
-            _dbContext.Commands.Remove(command);
+            _dbContext.Commands.UpdateRange(commands);
             await _dbContext.SaveChangesAsync();
             return Ok();
         }
+
+        // [HttpPost("add")]
+        // public async Task<IActionResult> AddCommandAsync(CommandDto dto)
+        // {
+        //     Command newCommand = new Command
+        //     {
+        //         Name = dto.Name,
+        //         Permission = dto.Permission,
+        //         IsEnabled = dto.IsEnabled,
+        //         DisplayInMenu = dto.DisplayInMenu
+        //     };
+
+        //     foreach (var alias in dto.Aliases)
+        //     {
+        //         newCommand.Aliases.Add(
+        //             new Command()
+        //             {
+        //                 Name = dto.Name,
+        //                 Permission = dto.Permission,
+        //                 IsEnabled = dto.IsEnabled,
+        //                 DisplayInMenu = dto.DisplayInMenu
+        //             }
+        //         );
+        //     }
+
+        //     _dbContext.Commands.Add(newCommand);
+        //     await _dbContext.SaveChangesAsync();
+
+        //     return Created("api/controller/add", dto);
+        // }
+
+        // [HttpDelete("delete")]
+        // public async Task<IActionResult> DeleteCommandAsync(int id)
+        // {
+        //     var command = await _dbContext.Commands.FirstOrDefaultAsync(c => c.Id == id);
+        //     if (command == null)
+        //     {
+        //         return BadRequest(new MessageResponseDto("Command not found."));
+        //     }
+
+        //     _dbContext.Commands.Remove(command);
+        //     await _dbContext.SaveChangesAsync();
+        //     return Ok();
+        // }
     }
 }
