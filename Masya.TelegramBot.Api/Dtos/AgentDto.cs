4bf8234fed6210862b826d4cff32be7f99ef35c8
Maksym Diachenko <i.am.masya1@gmail.com>
2021-09-09 22:22:41 +0300

    Updated dto

--- Masya.TelegramBot.Api/Dtos/AgentDto.cs
+++ Masya.TelegramBot.Api/Dtos/AgentDto.cs
@@ -1,12 +1,16 @@
+using Masya.TelegramBot.DataAccess.Models;
+
 namespace Masya.TelegramBot.Api.Dtos
 {
     public sealed class AgentDto
     {
+        public long Id { get; set; }
         public string TelegramLogin { get; set; }
         public string TelegramAvatar { get; set; }
         public string TelegramFirstName { get; set; }
         public string TelegramLastName { get; set; }
         public string TelegramPhoneNumber { get; set; }
+        public Permission Permission { get; set; }
         public bool IsBlocked { get; set; }
         public string BlockReason { get; set; }
         public bool? IsBlockedByBot { get; set; }
