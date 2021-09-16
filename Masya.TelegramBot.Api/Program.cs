3f9fde64031448e33539d5915f9a9ef937eb378c
Maksym Diachenko <i.am.masya1@gmail.com>
2021-09-16 21:08:08 +0300

    Updated program file

--- Masya.TelegramBot.Api/Program.cs
+++ Masya.TelegramBot.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using System;
 using System.Threading.Tasks;
@@ -9,39 +10,49 @@ using Masya.TelegramBot.Modules;
 using Masya.TelegramBot.DataAccess;
 using Masya.TelegramBot.DatabaseExtensions.Metadata;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Masya.TelegramBot.Api
 {
-  public static class Program
-  {
-    public static async Task Main(string[] args)
+    public static class Program
     {
-      var host = CreateHostBuilder(args).Build();
-      await StartRequiredServices(host.Services);
-      await host.RunAsync();
-    }
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            await StartRequiredServices(host.Services);
+            await host.RunAsync();
+        }
 
-    public static async Task StartRequiredServices(IServiceProvider services)
-    {
-      using var scope = services.CreateScope();
-      var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-      await ApplicationDbContext.SeedDatabase(dbContext);
-      var commandService = services.GetRequiredService<ICommandService<DatabaseCommandInfo, DatabaseAliasInfo>>();
-      var botService = services.GetRequiredService<IBotService<DatabaseCommandInfo, DatabaseAliasInfo>>();
-      await commandService.LoadCommandsAsync(typeof(BasicModule).Assembly);
-      await botService.SetWebhookAsync();
-    }
+        public static async Task StartRequiredServices(IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await ApplicationDbContext.SeedDatabase(dbContext);
+            var commandService = services.GetRequiredService<ICommandService<DatabaseCommandInfo, DatabaseAliasInfo>>();
+            var botService = services.GetRequiredService<IBotService<DatabaseCommandInfo, DatabaseAliasInfo>>();
+            await commandService.LoadCommandsAsync(typeof(BasicModule).Assembly);
+            await botService.SetWebhookAsync();
+        }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(loggingBuilder =>
-            {
-              loggingBuilder.AddSerilog();
-              loggingBuilder.AddAzureWebAppDiagnostics();
-            })
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
-  }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(configBuilder =>
+                {
+                    configBuilder
+                    .AddEnvironmentVariables()
+                    .AddJsonFile(
+                      path: Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"),
+                      optional: false,
+                      reloadOnChange: true
+                    );
+                })
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddSerilog();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
