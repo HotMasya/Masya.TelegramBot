ce44a811489b4c12d74c82f2ec45d1d73f48160b
Maksym Diachenko <i.am.masya1@gmail.com>
2021-09-18 18:14:31 +0300

    Updated log dto

--- Masya.TelegramBot.Api/Dtos/LogDto.cs
+++ Masya.TelegramBot.Api/Dtos/LogDto.cs
@@ -4,6 +4,7 @@ namespace Masya.TelegramBot.Api.Dtos
 {
     public sealed class LogDto
     {
+        public long Id { get; set; }
         public string Message { get; set; }
         public string Level { get; set; }
         public DateTime TimeStamp { get; set; }
