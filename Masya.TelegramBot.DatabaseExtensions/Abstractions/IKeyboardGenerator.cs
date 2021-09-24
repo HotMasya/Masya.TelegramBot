d352edf66318f8c42a422d69a1d5530ebe621f13
Maksym Diachenko <i.am.masya1@gmail.com>
2021-09-24 22:44:23 +0300

    Added semicolon

--- Masya.TelegramBot.DatabaseExtensions/Abstractions/IKeyboardGenerator.cs
+++ Masya.TelegramBot.DatabaseExtensions/Abstractions/IKeyboardGenerator.cs
@@ -11,7 +11,7 @@ namespace Masya.TelegramBot.DatabaseExtensions.Abstractions
         IServiceProvider Services { get; }
         CommandServiceOptions Options { get; }
         IReplyMarkup Menu(Permission userPermission);
-        Task<IReplyMarkup> InlineSearch(string callbackDataType = null)
+        Task<IReplyMarkup> InlineSearch(string callbackDataType = null);
         IReplyMarkup Roles();
     }
 }
