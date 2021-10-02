654f3d466a68afcc5411bde0e2991fe4ef8a742e
Maksym Diachenko <i.am.masya1@gmail.com>
2021-10-02 15:36:06 +0300

    Added images and phone

---
+++ Masya.TelegramBot.DataAccess/Migrations/20211002123546_Added images and phone.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Masya.TelegramBot.DataAccess.Migrations
+{
+    public partial class Addedimagesandphone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "RealtyObjects",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "RealtyObjects",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RealtyObjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_RealtyObjects_RealtyObjectId",
+                        column: x => x.RealtyObjectId,
+                        principalTable: "RealtyObjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_RealtyObjectId",
+                table: "Images",
+                column: "RealtyObjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "RealtyObjects");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "RealtyObjects");
+        }
+    }
+}
