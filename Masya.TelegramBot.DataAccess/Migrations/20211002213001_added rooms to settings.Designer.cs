85f6f2c1a48f4061b7722d072881c058015ba16a
Maksym Diachenko <i.am.masya1@gmail.com>
2021-10-03 00:30:13 +0300

    Added rooms to settings

--- Masya.TelegramBot.DataAccess/Migrations/20211002213001_added rooms to settings.Designer.cs
+++ Masya.TelegramBot.DataAccess/Migrations/20211002213001_added rooms to settings.Designer.cs
@@ -4,14 +4,16 @@ using Masya.TelegramBot.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Masya.TelegramBot.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211002213001_added rooms to settings")]
+    partial class addedroomstosettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Masya.TelegramBot.DataAccess/Migrations/20211002213001_added rooms to settings.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Masya.TelegramBot.DataAccess.Migrations
+{
+    public partial class addedroomstosettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxRoomsCount",
+                table: "UserSettings");
+
+            migrationBuilder.CreateTable(
+                name: "RoomUserSettings",
+                columns: table => new
+                {
+                    RoomsId = table.Column<int>(type: "int", nullable: false),
+                    UserSettingsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomUserSettings", x => new { x.RoomsId, x.UserSettingsId });
+                    table.ForeignKey(
+                        name: "FK_RoomUserSettings_Rooms_RoomsId",
+                        column: x => x.RoomsId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomUserSettings_UserSettings_UserSettingsId",
+                        column: x => x.UserSettingsId,
+                        principalTable: "UserSettings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomUserSettings_UserSettingsId",
+                table: "RoomUserSettings",
+                column: "UserSettingsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomUserSettings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxRoomsCount",
+                table: "UserSettings",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
