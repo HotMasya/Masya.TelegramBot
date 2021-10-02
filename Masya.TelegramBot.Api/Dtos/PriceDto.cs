6cd135da0fee4ed549600651ca52ac8e6226f4dc
Maksym Diachenko <i.am.masya1@gmail.com>
2021-10-02 10:12:41 +0300

    Updated dto

--- Masya.TelegramBot.Api/Dtos/PriceDto.cs
+++ Masya.TelegramBot.Api/Dtos/PriceDto.cs
@@ -3,7 +3,7 @@ namespace Masya.TelegramBot.Api.Dtos
     public sealed class PriceDto
     {
         public int? Id { get; set; }
-        public int MinValue { get; set; }
-        public int MaxValue { get; set; }
+        public int MinVal { get; set; }
+        public int MaxVal { get; set; }
     }
 }
